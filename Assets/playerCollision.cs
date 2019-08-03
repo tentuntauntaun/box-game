4875e1c442e3aa066434a81bc00488ce2bfd40f2
tentuntauntaun <stephen.sloss@gmail.com>
2019-08-03 19:23:19 +0100

    initial

---
+++ Assets/playerCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class playerCollision : MonoBehaviour {
+
+    public PlayerMovement movement;
+
+    private void OnCollisionEnter (Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+
+}
